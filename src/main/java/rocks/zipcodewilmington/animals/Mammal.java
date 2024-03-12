3aa3fc5f37af868786d7768fd3b1041acc2176aa
tb0902 <tbruton417@gmail.com>
2024-03-12 13:03:33 -0400

    Some tests passing

--- src/main/java/rocks/zipcodewilmington/animals/Mammal.java
+++ src/main/java/rocks/zipcodewilmington/animals/Mammal.java
@@ -9,45 +9,45 @@ import java.util.Date;
  * @author leon on 4/19/18.
  */
 public abstract class Mammal implements Animal {
-    private final Integer id;
-    private ArrayList<Food> eatenMeals;
-    private String name;
-    private Date birthDate;
+  private final Integer id;
+  private ArrayList<Food> eatenMeals;
+  private String name;
+  private Date birthDate;
 
-    public Mammal(String name, Date birthDate, Integer id) {
-        this.name = name;
-        this.birthDate = birthDate;
-        this.eatenMeals = new ArrayList<>();
-        this.id = id;
-    }
+  public Mammal(String name, Date birthDate, Integer id) {
+    this.name = name;
+    this.birthDate = birthDate;
+    this.eatenMeals = new ArrayList<>();
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public Date getBirthDate() {
-        return birthDate;
-    }
+  public Date getBirthDate() {
+    return birthDate;
+  }
 
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
+  public void setBirthDate(Date birthDate) {
+    this.birthDate = birthDate;
+  }
 
 
-    public Integer getNumberOfMealsEaten() {
-        return eatenMeals.size();
-    }
+  public Integer getNumberOfMealsEaten() {
+    return eatenMeals.size();
+  }
 
-    public void eat(Food food) {
-        eatenMeals.add(food);
-    }
+  public void eat(Food food) {
+    eatenMeals.add(food);
+  }
 
-    @Override
-    public Integer getId() {
-        return id;
-    }
+  @Override
+  public Integer getId() {
+    return id;
+  }
 }
--- src/test/java/rocks/zipcodewilmington/CatTest.java
+++ src/test/java/rocks/zipcodewilmington/CatTest.java
@@ -2,8 +2,11 @@ package rocks.zipcodewilmington;
 
 import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
 import rocks.zipcodewilmington.animals.Cat;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -21,23 +24,68 @@ public class CatTest {
 
     @Test
     public void constructorTest() {
-        // Given (cat data)
-        String givenName = "Zula";
-        Date givenBirthDate = new Date();
-        Integer givenId = 0;
+      // Given (cat data)
+      String givenName = "Zula";
+      Date givenBirthDate = new Date();
+      Integer givenId = 0;
 
-        // When (a cat is constructed)
-        Cat cat = new Cat(givenName, givenBirthDate, givenId);
+      // When (a cat is constructed)
+      Cat cat = new Cat(givenName, givenBirthDate, givenId);
 
-        // When (we retrieve data from the cat)
-        String retrievedName = cat.getName();
-        Date retrievedBirthDate = cat.getBirthDate();
-        Integer retrievedId = cat.getId();
+      // When (we retrieve data from the cat)
+      String retrievedName = cat.getName();
+      Date retrievedBirthDate = cat.getBirthDate();
+      Integer retrievedId = cat.getId();
 
-        // Then (we expect the given data, to match the retrieved data)
-        Assert.assertEquals(givenName, retrievedName);
-        Assert.assertEquals(givenBirthDate, retrievedBirthDate);
-        Assert.assertEquals(givenId, retrievedId);
+      // Then (we expect the given data, to match the retrieved data)
+      Assert.assertEquals(givenName, retrievedName);
+      Assert.assertEquals(givenBirthDate, retrievedBirthDate);
+      Assert.assertEquals(givenId, retrievedId);
     }
 
+    @Test
+    public void setNameTest() {
+      // Given (cat name)
+      String givenName = "Leo";
+      Cat cat = new Cat(null, null, null);
+
+      // When (a name is set)
+      cat.setName(givenName);
+
+      // The name is "Leo"
+      Assert.assertEquals(givenName, cat.getName());
+    }
+
+  @Test
+  public void setBirthDateTest() {
+    Date birthDay = new Date(1608888630000L);
+    Cat cat = new Cat(null, null, null);
+
+    cat.setBirthDate(birthDay);
+
+    Assert.assertEquals(birthDay, cat.getBirthDate());
+  }
+
+  @Test
+  public void speakTest() {
+    Cat cat = new Cat(null, null, null);
+
+    String actual = cat.speak(); // when you test a string (or any return value),
+    String expected = "meow!"; // there needs to be an expected and an actual
+
+    Assert.assertEquals(expected, actual); // make sure the expected == the actual
+  }
+
+  @Test
+  public void eatFoodTest() {
+    Cat cat = new Cat(null, null, null);
+    Food food = new Food();
+
+
+
+    cat.eat(food);
+    
+
+
+  }
 }
